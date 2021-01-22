88e749c4a0da01b3823816f7d57b0e0fbd5fea6e
vivek-kommi <73693505+vivek-kommi@users.noreply.github.com>
2021-01-22 16:20:07 +0000

    Update NDVI.py

--- NDVI.py
+++ NDVI.py
@@ -10,6 +10,16 @@ from datetime import datetime, timedelta
 import numpy as np
 import time
 
+
+y = [255,255,0]  # Yellow
+aq = [0,128,128]  # Teal (Ocean-Blue)
+m = [139,69,19]  # Sadle Brown
+g = [0,255,0]  # Lime green
+b = [0,0,128]  # Blue navy
+o = [0,0,0]  # Black
+r = [255,0,0]  # Red
+
+
 sat_data = rasterio.open('image.tif')
 b, r, g = sat_data.read()
 
@@ -23,12 +33,3 @@ fig = plt.imshow(g)
 
 fig.set_cmap('gist_earth')
 plt.show()
-
-
-y = [255,255,0]  # Yellow
-aq = [0,128,128]  # Teal (Ocean-Blue)
-m = [139,69,19]  # Sadle Brown
-g = [0,255,0]  # Lime green
-b = [0,0,128]  # Blue navy
-o = [0,0,0]  # Black
-r = [255,0,0]  # Red
